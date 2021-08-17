d77897cdf9f35b371114390faa3ed76ff7170a4e
bel-sndct <79698612+bel-sndct@users.noreply.github.com>
2021-08-17 13:59:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let num_str = '';
+    let number = n;
+    if (number < 0) {
+        number *= -1;
+    }
+    while (number !== 0) {
+        num_str += number % 10;
+        number = Math.floor(number / 10);
+    }
+    return parseInt(num_str);
 }
